141454817f14e113bb42198394491e4b20becb19
Pere <pere.solaclaver@gmail.com>
2019-10-14 21:42:00 +0100

    Test display component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,19 +1,1 @@
 // Test away!
-import React from 'react';
-import * as rtl from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Controls from './Controls';
-
-afterEach(rtl.cleanup);
-
-let wrapper;
-
-beforeEach(() => {
-    wrapper = rtl.render(<Controls />)
-});
-
-describe('Controls component', () => {
-    test('defaults to unlocked and open', () => {
-        expect(wrapper.container).toMatchSnapshot();
-    })
-})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+describe('Display component', () => {
+    it('displays if gate is open/closed and if it is locked/unlocked', () => {
+        const wrapper = rtl.render(<Display locked={false} closed={false}/>)
+        expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    })
+})
